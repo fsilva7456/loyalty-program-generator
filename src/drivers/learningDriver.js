86af9628d783036eebbbbfa6b684dbe607141492
Francis <32603796+fsilva7456@users.noreply.github.com>
2024-12-13 11:05:34 -0500

    feat: add learning driver

---
+++ src/drivers/learningDriver.js
@@ -0,0 +1,42 @@
+export const learningDriver = {
+  name: 'Learning',
+  description: 'This category focuses on providing educational opportunities, resources, and experiences that enhance the knowledge and skills of loyalty program members through workshops, webinars, courses, and informative content.',
+  subDrivers: {
+    contentRelevance: {
+      name: 'Relevance of Content',
+      description: 'Assesses how well educational materials align with members\'s interests, needs, and professional goals.'
+    },
+    accessibility: {
+      name: 'Accessibility of Learning Resources',
+      description: 'Evaluates ease of access to learning materials through various mediums and accommodates different learning styles and abilities.'
+    },
+    contentQuality: {
+      name: 'Quality of Educational Content',
+      description: 'Measures the caliber of learning resources, including instructor expertise and teaching effectiveness.'
+    },
+    formatDiversity: {
+      name: 'Diversity of Learning Formats',
+      description: 'Focuses on providing varied learning formats like videos, webinars, articles, and podcasts to suit different preferences.'
+    },
+    skillApplication: {
+      name: 'Opportunities for Skill Application',
+      description: 'Assesses availability of practical applications through projects, challenges, or real-world scenarios.'
+    },
+    supportSystems: {
+      name: 'Feedback and Support Systems',
+      description: 'Evaluates quality of learning support through Q&A sessions, forums, mentorship, and instructor feedback.'
+    },
+    progressTracking: {
+      name: 'Progress Tracking and Recognition',
+      description: 'Measures how the program tracks and recognizes member achievements through certificates, badges, or points.'
+    },
+    programClarity: {
+      name: 'Clarity of Program Workings',
+      description: 'Assesses how effectively the program communicates its learning components and benefits to members.'
+    },
+    personalization: {
+      name: 'Personalization of Learning Pathways',
+      description: 'Evaluates ability to tailor learning experiences to individual member needs, pace, and interests.'
+    }
+  }
+};
